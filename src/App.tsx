8ac57ad9ff7ac2c2df660418c74a95633e8eb119
AnnaKubiak <a.kubiak@wearevirtua.com>
2020-07-15 09:23:06 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalTemplete from './utils/GlobalTemplete';
+
+function App() {
+  return (
+    <GlobalTemplete>
+      <div>Ania</div>
+    </GlobalTemplete>
+  );
+}
+
+export default App;
